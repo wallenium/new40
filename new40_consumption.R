b16f7b34636988b14915d6f1d48d133eee68b95d
Simon W <s.wallmann@kompsoft.de>
2019-04-30 23:15:33 +0200

    Update new40_consumption.R
    Fixed the Calculation

--- new40_consumption.R
+++ new40_consumption.R
@@ -53,6 +53,13 @@ allData <- lapply(filenames, function(.file){
 myDF <- do.call(rbind, allData)
 myDF2 <- myDF
 myDF2$time <- as.POSIXct(myDF2$time)
+myDF2$usage <- as.numeric(as.character(myDF2$usage))
+myDF3 <- myDF2
+
+myDF2 <- myDF3 %>%
+  group_by(id) %>%
+  mutate(diff = usage - lag(usage, default = first(usage), order_by = time))
+debug_rohdaten <- myDF2 %>% group_by(month=floor_date(time, "month"), id) %>% summarize(amount=last(usage))
 
 # get all results for a start and end time
 # https://stackoverflow.com/questions/27206074/r-extracting-data-from-certain-date-or-time-period-from-a-dataframe
@@ -61,7 +68,8 @@ data_newtime_consumption <- data.frame(id=character(),
                                        date_start=numeric(),
                                        date_end=numeric(), 
                                        usage_start=double(),
-                                       usage_end=double()
+                                       usage_end=double(),
+                                       consumption=double()
                                        ) 
 for (j in 1:nrow(new_schaltzeiten)){
   new_time <- myDF2[myDF2$time>=as.POSIXct(new_schaltzeiten$start[j]) & myDF2$time<=as.POSIXct(new_schaltzeiten$end[j]),]
@@ -75,7 +83,13 @@ for (j in 1:nrow(new_schaltzeiten)){
                                       ))
   data_new_time  <- rbind(data_new_time , new_time)
 }
-data_newtime_consumption$consumption <- as.numeric(as.character(data_newtime_consumption$usage_end)) - as.numeric(as.character(data_newtime_consumption$usage_start))
+data_newtime_consumption$consumption <- as.numeric(as.numeric(as.character(data_newtime_consumption$usage_end)) - as.numeric(as.character(data_newtime_consumption$usage_start)))
+debug_newtime <- data_newtime_consumption %>% group_by(month=floor_date(date_start, "month"), id) %>% summarize(amount=sum(consumption))
+#debug_newtime2 <- data_new_time %>% group_by(month=floor_date(time, "month"), id) %>% summarize(amount=last(usage))
+
+#calculate 
+data_consumption_per_day <- myDF2 %>% group_by(month=floor_date(time, "day"), id) %>% summarize(total_consumption=sum(diff))
+
 data_new_oot <- subset(myDF2, !(time %in% data_new_time$time))
 data_new_oot_sum <- data_new_oot %>% 
                                     dplyr::group_by(day=floor_date(time, "day"),id) %>% 
@@ -83,9 +97,12 @@ data_new_oot_sum <- data_new_oot %>%
                                       date_start = dplyr::first(time),
                                       date_end = dplyr::last(time),
                                       usage_start = dplyr::first(usage),
-                                      usage_end = dplyr::last(usage)
+                                      usage_end = dplyr::last(usage),
+                                      consumption = (sum(diff))
                                     )
-data_new_oot_sum$consumption <- (as.numeric(as.character(data_new_oot_sum$usage_end)) - as.numeric(as.character(data_new_oot_sum$usage_start)) )
+#data_new_oot_sum$consumption <- (as.numeric(as.character(data_new_oot_sum$usage_end)) - as.numeric(as.character(data_new_oot_sum$usage_start)) )
+debug_ootime <- data_new_oot_sum %>% group_by(month=floor_date(day, "month"), id) %>% summarize(amount=sum(consumption))
+debug_ootime2 <- data_new_oot %>% group_by(month=floor_date(time, "month"), id) %>% summarize(amount=last(usage))
 
 ggplot(new_schaltzeiten, aes(x=start, y=as.numeric(diff_mins)))+
   geom_line()+
@@ -93,18 +110,18 @@ ggplot(new_schaltzeiten, aes(x=start, y=as.numeric(diff_mins)))+
   scale_x_datetime(name="Datum", date_breaks = "10 days") +
   geom_hline(aes(yintercept = mean(as.numeric(diff_mins))), color="blue")
 
-
-data_new_it_sum <- data_newtime_consumption %>% group_by(day=floor_date(date_start, "day"), id) %>%
-  summarize(amount=sum(consumption))
+data_new_it_sum <- data_newtime_consumption %>% group_by(day=floor_date(date_start, "day"), id) %>% summarize(Billig=sum(consumption))
+debug_newit <- data_new_it_sum %>% group_by(month=floor_date(day, "month"), id) %>% summarize(Billig=sum(Billig))
 data_new_oot2_sum <- data_new_oot_sum
 data_new_oot2_sum$date_start <- NULL
 data_new_oot2_sum$date_end <- NULL
 data_new_oot2_sum$usage_start <- NULL
 data_new_oot2_sum$usage_end <- NULL
+names(data_new_oot2_sum)[3] <- "Teuer"
 
-consumptionPerDay <- merge(data_new_it_sum, data_new_oot2_sum, by = c("day", "id") )
-names(consumptionPerDay)[3] <- "Billig"
-names(consumptionPerDay)[4] <- "Teuer"
+consumptionPerDay <- merge(data_new_it_sum, data_new_oot2_sum, by = c("day", "id"), all = TRUE)
+consumptionPerDay[is.na(consumptionPerDay)] <- 0
+#debug_cpd <- consumptionPerDay %>% group_by(month=floor_date(day, "month"), id) %>% summarize(Billig=sum(Billig), Teuer=sum(Teuer))
 
 # rename the ids
 consumptionPerDay$id[consumptionPerDay$id %in% "NEW4.0 #4"] <- "Waschmaschine"
@@ -126,3 +143,6 @@ ggplot(aes(x = id, y = value, fill = variable)) +
 consumptionPerDay$percentageCheap <- round(consumptionPerDay$Billig/(consumptionPerDay$Billig+consumptionPerDay$Teuer), 2)
 percentagePerMonthCheap <- consumptionPerDay %>% group_by(day=floor_date(day, "month"), id) %>% summarize(Billig=sum(Billig), Teuer=sum(Teuer))
 percentagePerMonthCheap$percentageCheap <- round(percentagePerMonthCheap$Billig/(percentagePerMonthCheap$Billig+percentagePerMonthCheap$Teuer), 2)
+
+# save
+write.csv(x = consumptionPerDay, file = "~/Documents/Programmierung/R-Scripts/New40/exported_data.csv")
